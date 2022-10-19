9c8b98a733b0b56bed846ba4c2c770a31c286d44
SayGoodnight <102477210+YuriiPohorilets@users.noreply.github.com>
2022-10-19 21:14:22 +0300

    add no poster images

--- src/js/templates/movieList.js
+++ src/js/templates/movieList.js
@@ -1,20 +1,41 @@
 import refs from '../refs/refs';
+import noPosterImage from '../../images/no-poster.jpeg';
+
+let imgBaseUrl = 'https://image.tmdb.org/t/p/w500/';
+const noPosterImg = noPosterImage;
 
 export default function renderMovieListMarkup(movies) {
-  const imgBaseUrl = 'https://image.tmdb.org/t/p/w500/';
-
-  const movieList = movies
-    .map(({ backdrop_path, title }) => {
-      return `
-  <li class="movie__item">
-    <a hres="#" class="movie__link">
-      <img class="movie__img" src="${imgBaseUrl}${backdrop_path}" />
-      <p class="movie__title">${title}</p>
-    </a>
-  </li>
-  `;
+  const moviesList = movies
+    .map(movie => {
+      if (movie.poster_path === null) {
+        imgBaseUrl = '';
+        movie.poster_path = noPosterImg;
+      } else {
+        imgBaseUrl = 'https://image.tmdb.org/t/p/w500/';
+      }
+      return moviesListTemplate(movie);
     })
     .join('');
 
-  refs.moviesList.insertAdjacentHTML('beforeend', movieList);
+  refs.moviesList.insertAdjacentHTML('beforeend', moviesList);
+}
+
+function moviesListTemplate({ poster_path, title, release_date }) {
+  return `
+        <li class="movie__item">
+          <a hres="#" class="movie__link">
+            <img class="movie__img"
+            loading="lazy"
+            src="${imgBaseUrl}${poster_path}" 
+
+            />
+            
+            <div class="movie__descr">
+              <p class="movie__title">${title}</p>
+              <p class="movie__genre">${title}</p>
+              <p class="movie__year">${release_date}</p>
+            </div>
+          </a>
+        </li>
+  `;
 }
